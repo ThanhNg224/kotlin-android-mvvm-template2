0e1897ae3ac188d8a6ea0f186c7f202d312c8e50
Nguyen Phuc Thanh <131433015+ThanhNg224@users.noreply.github.com>
2025-11-17 14:10:46 +0700

    Remove unused imports in App.kt
    Cleaned up App.kt by removing several unused import statements related to icons and ImageVector. This helps reduce clutter and improve code readability.

--- app/src/main/java/com/thanhng224/app/presentation/App.kt
+++ app/src/main/java/com/thanhng224/app/presentation/App.kt
@@ -9,11 +9,6 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Favorite
-import androidx.compose.material.icons.filled.Home
-import androidx.compose.material.icons.filled.Person
-import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material3.Badge
 import androidx.compose.material3.BadgedBox
 import androidx.compose.material3.CircularProgressIndicator
@@ -33,7 +28,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.graphicsLayer
-import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.unit.dp
 import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
